778aba8071247d1762e4c85c6afb1e8c6de532f9
Peter Cuevas <petercuevas.6@gmail.com>
2021-04-04 15:53:20 -0500

    :)

---
+++ ad_filter2hosts.sh
@@ -0,0 +1,18 @@
+ #!/bin/bash
+ for source in `cat adblock_lists.lst`; do
+     echo $source;
+     curl --silent $source >> ads.txt
+     echo -e "\t`wc -l ads.txt | cut -d " " -f 1` lines downloaded"
+ done
+ 
+ echo -e "\nFiltering non-url content..."
+ perl convert.pl ads.txt > ads_parsed.txt
+ rm ads.txt
+ echo -e "\t`wc -l ads_parsed.txt | cut -d " " -f 1` lines after parsing"
+ 
+ echo -e "\nRemoving duplicates..."
+ sort -u ads_parsed.txt > adsblock_list.txt
+ rm ads_parsed.txt
+ echo -e "\t`wc -l ads_unique.txt | cut -d " " -f 1` lines after deduping"
+ 
+ echo -e "\nDone!"
